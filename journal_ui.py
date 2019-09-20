d4a0d08cdd3c477f291ccab598587f8a30728955
Will Vollmer <vollmerwill01@gmail.com>
2019-09-20 14:12:31 -0400

    get basic journal working

--- journal_ui.py
+++ journal_ui.py
@@ -1,3 +1,4 @@
+import journal
 from banner import banner
 banner("deep thoughts", "william vollmer")
 
@@ -6,30 +7,34 @@ def main():
 
 
 def run_event_loop():
-    journal_data = []
+    filename = 'default'
+    journal_data = journal.load(filename)
 
     while True:
         command = input("[L]ist entries, [A]dd an entry, E[x]it: ")
 
-        if command.upper()== "L":
+        if command.upper() == "L":
             list_entries(journal_data)
         elif command.upper() == "A":
             add_entry(journal_data)
         elif command.upper() == "C":
             print("=)")
+            break
         elif command.upper() == "X":
             break
         else:
             print("sorry i dont understand")
+    journal.save(filename, journal_data)
 
 def list_entries(data):
-    print("your journal entries:")
+    print("your journal entries: ")
     entries = reversed(data)
-    for  num, entry in enumerate(entries):
-        print(f"{num+1}" - "{entry}")
+    for num, entry in enumerate(entries):
+        print(f"{num+1} - {entry}")
 def add_entry(data):
     entry = input("Type your entry, <ENTER> to exit: \n")
-    data.append(entry)
+    journal.add_entry(entry, data)
+    #data.append(entry)
 
 
 main()
